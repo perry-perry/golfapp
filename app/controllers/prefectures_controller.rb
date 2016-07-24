0f46715c37ba7cb93c8d95c441626d421c81add6
Perry <perry.luman@gmail.com>
2016-07-24 15:48:43 +0900

    Finish Prefecture

--- app/controllers/prefectures_controller.rb
+++ app/controllers/prefectures_controller.rb
@@ -1,2 +1,50 @@
 class PrefecturesController < ApplicationController
+	
+before_action :set_prefecture, only:[:show,:edit,:update,:destroy]
+
+def index
+	@prefectures = Prefecture.all
+end
+
+def show 
+end
+
+def new
+	@prefecture = Prefecture.new
+end
+
+def create
+	@prefecture = Prefecture.new(prefecture_params)
+	@prefecture.save
+	redirect_to prefectures_path
+end
+
+def edit
+end
+
+def update
+	@prefecture.update(prefecture_params)
+	redirect_to prefectures_path
+end
+
+def destroy
+	@prefecture.destroy
+	redirect_to prefectures_path
+end
+
+
+
+
+
+private
+
+ def prefecture_params
+ 	params[:prefecture].permit(:name)
+ end
+
+def set_prefecture
+	@prefecture = Prefecture.find(params[:id])
+end
+
+
 end
